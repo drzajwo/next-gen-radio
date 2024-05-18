5fdfee225efee2155c7305752fcdcf7c0cfbdda5
Iwo Wojciechowski <iwo.wojciechowski@caspio.com>
2024-05-18 13:09:33 +0200

    Project cleanup. Added header bar component

---
+++ src/locales/common.ts
@@ -0,0 +1,10 @@
+export enum CommonLocales {
+  TITLE = "TITLE",
+  DESCRIPTION = "DESCRIPTION",
+}
+
+export const commonLocales: Record<CommonLocales, string> = {
+  [CommonLocales.TITLE]: "Next gen radio",
+  [CommonLocales.DESCRIPTION]:
+    "This is aspiring next generation online radio to listen anywhere!",
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
